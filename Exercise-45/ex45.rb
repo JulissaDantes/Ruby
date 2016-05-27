9bc20399e0a96ec199a96e8ed407e901b57796b1
JulissaDantes <julissadcj@gmail.com>
2016-05-27 16:00:03 -0400

    Main class
    Where the magic happens

---
+++ Exercise-45/ex45.rb
@@ -0,0 +1,16 @@
+require "./room.rb"
+
+print "\t \t \t \t \n \n The Math Hall\n"
+puts "You are locked in a hall with 4 doors leading to 4 different rooms and one exit, Baldor's ghost is here to help you, but if you are not "
+items = []
+$pasillo = Hall.new()
+actions << $pasillo.play()
+#if action is something unleash the next events
+
+#if a 0 is the last element of the items array it means the player is dead
+while items.last != 0
+	
+
+
+
+end
